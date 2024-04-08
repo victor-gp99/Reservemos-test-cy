8ba1cc92ebb0b2dd99ca435612cfc550673784f1
Lalo Garcia <victoreduardogpat99@outlook.com>
2024-04-08 01:04:05 -0500

    ADD: final_test

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Reservemos-test-cy
+# Cypress Testing Project
+
+This project uses Cypress to automate a simple purchase test flow on the travel booking site https://roll-bits.reservamos-saas.com/.
+
+## Code
+
+The main code is located in the `spec.cy.js` file. This file contains a series of functions that interact with the elements on the web page.
+
+## Limitations
+
+Due to the same-origin policy (CORS) implemented in web browsers, Cypress cannot directly interact with elements within an iframe that is on a different domain. This is a known limitation of Cypress and can cause errors when running the script. For this reason, the code gives an error when looking for the element `cy.wrap(iframe.find('input[data-fieldtype="encryptedCardNumber"]')).type(encryptedCardNumber);`.
+I tried several solutions, such as waiting for the iframe to load completely and verifying that the selectors of the elements are correct. However, these solutions have not solved the problem due to the same-origin policy.
+There are alternative solutions such as: configuring a proxy server so that the iframe content appears to come from the same origin with Burpsuite, or simulating user actions by sending the corresponding events. However, these solutions can be complicated to implement and may not be the best option for a test.
+The most viable option would be the use of a different testing tool that does not have this limitation, such as Selenium. However, this would require a significant change in the approach to testing and I have no experience with Selenium, I can learn it if necessary.
+
+## How to run the script
+
+To run the script, follow these steps:
+
+1. Make sure you have Node.js and npm installed on your system. You can download them from here.
+
+2. Clone this repository to your local system.
+
+3. Navigate to the project directory.
+
+4. Run the following command to install Cypress dependencies:
+
+    ```bash
+    npm install
+    ```
+
+5. Run the following command to open the Cypress interface:
+
+    ```bash
+    npx cypress open
+    ```
+
+6. In the Cypress interface that opens, select the `spec.cy.js` file to run the script.
+
+**Please note that due to Cypress's limitations with iframes from different origins, some parts of the script may not execute correctly.**
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,73 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Code Challenge: Simple purchases', function() {
+  
+  beforeEach(function() { 
+    cy.fixture('testData').then((data) => {
+      this.data = data;
+    });
+    cy.visit('https://roll-bits.reservamos-saas.com/');
+  });
+
+  it('Simple purchase for CDMX-LEON', function() { 
+    selectOriginDestination(this.data.origin, this.data.destination);
+    selectSeats();
+    fillPassengerInformation(this.data.name, this.data.last_name, this.data.email);
+    fillPaymentMethod(this.data.encryptedCardNumber, this.data.encryptedExpiryDate, this.data.encryptedSecurityCode,this.data.name, this.data.last_name);
+  });
+
+  function selectOriginDestination(origin, destination) {
+    cy.get('input#txtorigin-desktop').click({force:true});
+    cy.get('div.place-info').contains(origin).click({force:true});
+    cy.get('input#txtdestination-desktop').click({force:true});
+    cy.get('div.place-info').contains(destination).click({force:true});
+    cy.get('.dates-controls-wrapped > :nth-child(2)').click({force:true});
+    cy.get('.search-button').click({force:true});
+    cy.get('.results-title').should('contain', 'Seleccionar horario de ida');
+  }
+  function selectSeats() {
+    cy.get('div.matrix-action button').eq(1).click({force:true});
+    cy.wait(1000);
+    cy.get('.selected-seats').should('contain', 'Elige los asientos que necesites');
+    cy.get('.seat-available').then(($seats) => {
+      const randomIndex = Math.floor(Math.random() * $seats.length);
+      cy.wrap($seats[randomIndex]).click({force:true});
+    });
+    cy.get('button.main-button').click({force:true});
+  }
+  function fillPassengerInformation(name, lastName, email) {
+    cy.get('div.passengers-heading').should('contain', 'Pasajero');
+    cy.get('input[placeholder="Nombre (s)"]').type(name);
+    cy.get('input[placeholder="Apellido Paterno"]').type(lastName);
+    cy.get('input[placeholder="Correo electrónico"]').type(email);
+    cy.get('button.main-button').click({force:true});
+  }
+  //Here we are using the window object to stub the postMessage function that is used to communicate with the Adyen iframe but the 
+  //CORS policy is blocking the communication
+  function fillPaymentMethod(encryptedCardNumber, encryptedExpiryDate, encryptedSecurityCode, name, lastName) {
+    cy.wait(10000);
+    cy.get('input#check-passengers').check({force:true});
+    let randomPhoneNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+    cy.get('input#phone').type(randomPhoneNumber.toString());
+    cy.window().then((win) => {
+      cy.stub(win, 'postMessage').callsFake((message, targetOrigin) => {
+        if (targetOrigin === 'https://secure.adyen.com') {
+          // Simula la interacción con los campos seguros
+          if (message.action === 'encryptedCardNumber') {
+            return Promise.resolve({ data: 'encryptedCardNumber' });
+          } else if (message.action === 'encryptedExpiryDate') {
+            return Promise.resolve({ data: 'encryptedExpiryDate' });
+          } else if (message.action === 'encryptedSecurityCode') {
+            return Promise.resolve({ data: 'encryptedSecurityCode' });
+          }
+        }
+      });
+    });
+    cy.get('iframe.js-iframe').then($iframe => {
+      const iframe = $iframe.contents();
+      cy.wrap(iframe.find('input[data-fieldtype="encryptedCardNumber"]')).type(encryptedCardNumber);
+      cy.wrap(iframe.find('input[data-fieldtype="encryptedExpiryDate"]')).type(encryptedExpiryDate);
+      cy.wrap(iframe.find('input[data-fieldtype="encryptedSecurityCode"]')).type(encryptedSecurityCode);
+  });
+    cy.get('input.adyen-checkout__input.adyen-checkout__input--text.adyen-checkout__card__holderName__input').type(name + ' ' + lastName);
+    cy.get('button.main-button').click();
+  }
+});
